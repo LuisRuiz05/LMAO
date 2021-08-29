694063bc81e96ea74138c39eca48829f30b84b77
LuisRuiz05 <0224451@up.edu.mx>
2021-08-29 14:42:26 -0500

    Test Pre-Alpha 0.0.2
    Added a pause menu.
    Added music to the game.
    Added a round minimap.

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pauseUI;
+    public AudioSource music;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+        music.Play();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Pause()
+    {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+        music.Pause();
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void Menu()
+    {
+        //Time.timeScale = 1f;
+        //load scene
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
